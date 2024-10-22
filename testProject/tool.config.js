75a8fbb276bbd578792fb5fd173b85f85b7427a6
huytran845~ <huytrit@gmail.com>
2024-10-21 18:32:08 -0700

    Converted the tool to use the cosmiconfig library to find  the configuration for the test project

---
+++ testProject/tool.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 6666,
+};
+//This project would use commonJS for the config since cosmiconfig was made to read commonJS files.
--- tool/src/commands/config-mgr.js
+++ tool/src/commands/config-mgr.js
@@ -1,23 +1,35 @@
 import chalk from "chalk";
-import { pkgUpSync } from "pkg-up"; //Using the pkg-up library to find the package.json file up the directory tree.
-import { promises as fs } from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import { cosmiconfigSync } from "cosmiconfig";
+// import { pkgUpSync } from "pkg-up"; //Using the pkg-up library to find the package.json file up the directory tree.
+//import { promises as fs } from "fs";
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+const configLoader = cosmiconfigSync("tool");
 
 async function getConfig() {
-  const pkgPath = pkgUpSync({ cwd: process.cwd() });
-  const pkgJson = await fs.readFile(pkgPath, "utf-8");
-  const pkg = JSON.parse(pkgJson);
+  // const pkgPath = pkgUpSync({ cwd: process.cwd() }); //Old code for using pkgUpSync t o find the file path
+  // const pkgJson = await fs.readFile(pkgPath, "utf-8");
+  // const pkg = JSON.parse(pkgJson);
 
-  if (pkg.tool) {
-    console.log("Found configuration", pkg.tool);
-    return pkg.tool;
-  } else {
+  // if (pkg.tool) {
+  //   console.log("Found configuration", pkg.tool);
+  //   return pkg.tool;
+  // } else {
+  //   console.log(chalk.yellow("Couldn't find configuration, using default"));
+  //   return { port: 1234 };
+  // }
+
+  const result = configLoader.search(process.cwd());
+
+  if (!result) {
     console.log(chalk.yellow("Couldn't find configuration, using default"));
     return { port: 1234 };
+  } else {
+    console.log("Found configuration", result.config);
+    return result.config;
   }
 }
 
